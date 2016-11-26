704117d51060866b41ad7e767c41fb06418dad45
oomella <oomchristina2013@gmail.com>
2016-11-26 16:52:42 +0700

    เพิ่ม header ,footer

---
+++ application/views/layout/footer_view.php
@@ -0,0 +1,6 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="<?php echo base_url('bower_components/font-awesome/css/font-awesome.min.css') ?>"</script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?php echo base_url('bower_components/font-awesome/css/font-awesome.min.css') ?>"></script>
+</body>
+</html>
---
+++ application/views/layout/test_view.php
@@ -0,0 +1,10 @@
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam atque ea est et fuga in, inventore, libero
+    minima minus nam nostrum odio omnis porro provident, quo quod quos sed.
+</div>
+<div>A alias aut corporis debitis et, illo incidunt itaque iusto labore necessitatibus nihil obcaecati odio quas quasi
+    quia ratione reiciendis, similique. Cumque est in ipsa natus perspiciatis quia sit tempora?
+</div>
+<div>A eius et necessitatibus obcaecati quis? At consequuntur corporis deleniti distinctio doloribus error esse fuga
+    inventore ipsam magni nisi possimus quasi repellat repudiandae, soluta! Commodi facilis impedit ipsam maiores
+    obcaecati.
+</div>
